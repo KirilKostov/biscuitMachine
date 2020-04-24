f13588c068c2dfd98c1bd6182768a3046607ebeb
Kiril Chitinov <k.chitinov@gmail.com>
2020-04-25 01:07:46 +0300

    Refactor heater logic

--- biscuitMachine.js
+++ biscuitMachine.js
@@ -2,10 +2,14 @@ const SWITCH_VALUES = ['off', 'pause', 'on']
 const MIN_BAKING_TEMPERATURE = 220
 const MAX_BAKING_TEMPERATURE = 240
 
+let machineIsPaused = false
 let conveyorIsOn = false
 let ovenIsOn = false
+let heaterIsOn = false
 let currentOvenTemperature = 0
 let biscuitsCount = 0
+let extruderPulseCount = 0
+let stamperPulseCount = 0
 
 const setBiscuitMachineState = machineSwitch => {
   if (validateSwitchValue(machineSwitch)){
@@ -33,42 +37,58 @@ const machineMotor = machineSwitch => {
       startOven()
       break
     case 'pause':
-      conveyorIsOn = false
+      pauseMachine()
       break
     default:
       stopOven()
   }
 }
 
-const bake = () => {
-  return new Promise(resolve => setTimeout(resolve, 350))
-}
-
-const temperatureRampTime = () => {
-  return new Promise(resolve => setTimeout(resolve, 150))
+const timeOut = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 const startOven = async () => {
+  if (machineIsPaused) {
+    machineIsPaused = false
+  }
+
   if (ovenIsOn) {
     console.log('Oven is already on')
     return false
   }
+
   ovenIsOn = true
-  conveyorIsOn = true
+  heaterIsOn = true
 
   while (ovenIsOn) {
-    await temperatureRampTime()
-    currentOvenTemperature ++
-    console.log('Oven heating. Current temperature: ' + currentOvenTemperature)
-    if (currentOvenTemperature >= MIN_BAKING_TEMPERATURE && currentOvenTemperature <= MAX_BAKING_TEMPERATURE && conveyorIsOn) {
-      await bake(500)
-      biscuitsCount ++
-      console.log('Current biscuitsCount: ' + biscuitsCount)
+    if (heaterIsOn) {
+      await timeOut(50)
+      currentOvenTemperature ++
+      console.log('Heater is ON, current oven temperature is: ' + currentOvenTemperature)
+    } else {
+      await timeOut(100)
+      currentOvenTemperature --
+      console.log('Heater is OFF, current oven temperature is: ' + currentOvenTemperature)
+    }
+    if (currentOvenTemperature >= MAX_BAKING_TEMPERATURE) {
+      heaterIsOn = false
+      console.log('Maximum baking temperature reached, turning heater off')
+    }
+    if (currentOvenTemperature <= MIN_BAKING_TEMPERATURE && !heaterIsOn) {
+      heaterIsOn = true
+      console.log('Turning heater on')
     }
 
-    if (currentOvenTemperature === MAX_BAKING_TEMPERATURE) {
-      console.log('Oven reached maximum temperature. Shutting down due to danger of overheating.')
-      stopOven()
+    conveyorIsOn = currentOvenTemperature >= MIN_BAKING_TEMPERATURE && !machineIsPaused
+    if (conveyorIsOn) {
+      extruderPulseCount ++
+      stamperPulseCount ++
+      await timeOut(200)
+      biscuitsCount ++
+      console.group('Baking :)')
+        console.log('Biscuits baked: ' + biscuitsCount)
+      console.groupEnd()
     }
   }
 }
@@ -80,11 +100,21 @@ const stopOven = async () => {
   }
 
   ovenIsOn = false
+  heaterIsOn = false
   conveyorIsOn = false
-  while(!ovenIsOn && currentOvenTemperature > 0) {
-    await temperatureRampTime()
+
+  while (!ovenIsOn && currentOvenTemperature > 0) {
+    await timeOut(100)
     currentOvenTemperature --
     console.log('Oven cooling. Current temperature: ' + currentOvenTemperature)
   }
   return false
+}
+
+const pauseMachine = () => {
+  if (machineIsPaused) {
+    console.log('Machine is already paused')
+    return false
+  }
+  machineIsPaused = true
 }
